cf03f01f1f29eb59e7470e4121a0ca5bf3a36edb
IInnffiinniittyy <tanuj813@gmail.com>
2022-11-13 05:07:28 +0530

    Pushing the code

--- AutomatedAuctionSystem/src/com/AuctionApp/Dao/AdministratorDao.java
+++ AutomatedAuctionSystem/src/com/AuctionApp/Dao/AdministratorDao.java
@@ -1,5 +1,10 @@
 package com.AuctionApp.Dao;
 
+import com.AuctionApp.Exceptions.AdministratorException;
+import com.AuctionApp.JavaBeanFiles.Administrator;
+
 public interface AdministratorDao {
+	
+	public Administrator loginAdministrator(String ausername, String apassword) throws AdministratorException;
 
 }
--- AutomatedAuctionSystem/src/com/AuctionApp/Dao/AdministratorDaoImpl.java
+++ AutomatedAuctionSystem/src/com/AuctionApp/Dao/AdministratorDaoImpl.java
@@ -1,5 +1,48 @@
 package com.AuctionApp.Dao;
 
-public class AdministratorDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.AuctionApp.Exceptions.AdministratorException;
+import com.AuctionApp.JavaBeanFiles.Administrator;
+import com.AuctionApp.Utility.DBUtil;
+
+public class AdministratorDaoImpl implements AdministratorDao {
+
+	@Override
+	public Administrator loginAdministrator(String ausername, String apassword) throws AdministratorException {
+		Administrator admin = null;
+		
+		try(Connection conn= DBUtil.provideConnection()) {
+			
+			PreparedStatement ps = conn.prepareStatement("Select * from Administrator Where aEmail = ? AND aPassword = ?");
+			
+			ps.setString(1, ausername);
+			ps.setString(2, apassword);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+		    	int id = rs.getInt("aID");
+		    	String name = rs.getString("aName");
+		    	String email = rs.getString("aEmail");
+		    	String password = rs.getString("apassword");
+		    	admin = new Administrator(id, name, email, password);
+		    	
+		    }else {
+		      throw new AdministratorException("Invalid Username or Password");
+		    }
+			
+			
+		} catch (SQLException e) {
+			throw new AdministratorException(e.getMessage());
+		}
+		
+		
+		
+		return admin;
+	}
 
 }
